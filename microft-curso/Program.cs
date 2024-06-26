17f032481d16e2e32a14f82aefb1522c380ab4d1
franciscoj <frankmazta@gmail.com>
2024-06-26 14:17:05 -0400

    este curso se esta siendo en microft

---
+++ microft-curso/Program.cs
@@ -0,0 +1,124 @@
+﻿
+/*
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+   if ((roll1 == roll2) && (roll2 == roll3))
+    {
+       Console.WriteLine("You rolled triples!  +6 bonus to total!");
+       total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+
+else if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+else
+{
+    Console.WriteLine("Sorry, you lose.");
+}
+*/
+
+
+/*
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}*/
+
+
+
+/*string[] fraudulentOrderIDs = new string[3];
+
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"primero: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+*/
+
+
+/*string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+
+
+/*
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+   sum += items; 
+   bin++;
+   Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"tengo {sum} items in inventory");
+*/
+
+
+string[]fraudulentos ={"B123", "C234","A345","C15","B177","G3003","C235","B179"};
+foreach (string atrapado in fraudulentos)
+{
+    if (atrapado.StartsWith("C"))
+    {
+     Console.WriteLine($"Numero fradulentos es: {atrapado}");
+    }  
+}
--- proyecto.net/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// internal class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         Console.WriteLine("hola pendejo");
-//     }
-// }
-
